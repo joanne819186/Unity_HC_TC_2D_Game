7acb983753b7dfca6d6d7d55e99eb215afeb9193
KID <50666207+KID421@users.noreply.github.com>
2020-12-03 15:54:57 +0800

    控制

---
+++ 2DGame/Assets/Scripts/MagicCube.cs
@@ -0,0 +1,57 @@
+﻿using System.Threading;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class MagicCube : MonoBehaviour
+{
+    private RectTransform rect;
+
+    private float tempSpeed;
+    private float timer;
+
+    private void Awake()
+    {
+        rect = GetComponent<RectTransform>();
+    }
+
+    private void Update()
+    {
+        Move();
+        Control();
+        Rotate();
+    }
+
+    private void Move()
+    {
+        if (timer < MagicCubeManager.speed)
+        {
+            timer += Time.deltaTime;
+        }
+        else
+        {
+            timer = 0;
+            rect.anchoredPosition += new Vector2(0, -10);
+        }
+    }
+
+    private void Control()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            tempSpeed = MagicCubeManager.speed;
+            MagicCubeManager.speed = 0.1f;
+        }
+        else if (Input.GetKeyUp(KeyCode.S))
+        {
+            MagicCubeManager.speed = tempSpeed;
+        }
+    }
+
+    private void Rotate()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            transform.eulerAngles -= new Vector3(0, 0, 90);
+        }
+    }
+}
---
+++ 2DGame/Assets/Scripts/MagicCubeManager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class MagicCubeManager : MonoBehaviour
+{
+    [Header("魔術方塊")]
+    public RectTransform[] magicCibes;
+    [Header("畫布")]
+    public Transform canvas;
+
+    private int indexCurrent;
+    private int indexNext = -1;
+    private RectTransform cubeCurrent;
+
+    public static float speed = 1;
+    public static float controlHorizontal = 150;
+    public static float controlSpace = 200;
+
+    private void Awake()
+    {
+        RandomMagicCube();
+    }
+
+    private void RandomMagicCube()
+    {
+        if (indexNext == -1) indexCurrent = Random.Range(0, magicCibes.Length);
+        else indexCurrent = indexNext;
+
+        cubeCurrent = Instantiate(magicCibes[indexCurrent], canvas).GetComponent<RectTransform>();
+
+        cubeCurrent.anchoredPosition = new Vector2(0, 400);
+
+        indexNext = Random.Range(0, magicCibes.Length);
+    }
+}
