43bbeed732725ae0646da85de373ce0ae08f4150
KID <50666207+KID421@users.noreply.github.com>
2020-12-03 21:21:23 +0800

    認識四大類型

--- 2DGame/Assets/Scripts/Car.cs
+++ 2DGame/Assets/Scripts/Car.cs
@@ -6,4 +6,15 @@ public class Car : MonoBehaviour
     // 數值 = 欄位 Field
     // 語法：
     // 類型 名稱；
+
+    // 四大類型 - Unity 常用
+    // 整　數 int：沒有小數點的數值，例：０、１、－９９
+    // 浮點數 float：有小數點的數值，例：０．１、０．１２３４５６、－１．９
+    // 字　串 string：ＢＭＷ、賓士、＠＃＄
+    // 布林值 bool：有 true、沒有 false
+
+    int size;
+    float weight;
+    string brand;
+    bool hasWindow;
 }
