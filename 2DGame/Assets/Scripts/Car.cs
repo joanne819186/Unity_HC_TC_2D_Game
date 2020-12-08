02c3834ef2129a75931be703a0d7d42602fab07a
joanne819186 <74964759+joanne819186@users.noreply.github.com>
2020-12-08 21:32:08 +0800

    1208練習程式書寫

--- 2DGame/Assets/Scripts/Car.cs
+++ 2DGame/Assets/Scripts/Car.cs
@@ -1,35 +1,38 @@
 ﻿using UnityEngine;
 
-public class Car : MonoBehaviour
-{
-    // 單行註解 (說明)
-    // 數值 = 欄位 Field
-    // 語法：
-    // 修飾詞 類型 名稱 (指定 值)；
+public class Car : 1208work
 
-    // 四大類型 - Unity 常用
-    // 整　數 int：沒有小數點的數值，例：０、１、－９９
-    // 浮點數 float：有小數點的數值，例：０．１、０．１２３４５６、－１．９
-    // 字　串 string：ＢＭＷ、賓士、＠＃＄
-    // 布林值 bool：有 true、沒有 false
+    //[Header("最高分數")]
+    public int scoreHigh;
+    //[Header("分數")]
+    public int 
+    //[Header("等級")]
+    public string v1;
+    //[Header ("方塊掉落音效)]
+    public AudioClip soundFall;
+    //[Header("方塊移動與旋轉音效)]
+    public AudioClip soundMove;
+    //Header("方塊消除聲音")]
+    public AudioClip soundClear;
+    //[Header("遊戲結束音效")]
+    public AudioClip soundEnd;
 
-    // 指定符號 =
-
-    // 私人 private 不會顯示在屬性面板上 - 預設值
-    // 公開 public  會顯示在屬性面板上
+    
 
 
-    // 欄位的屬性
-    // 標題 Header  - 字串
-    // 提示 Tooltip - 字串
-    // 範圍 Range
-    [Header("這是車子的尺寸")]
-    public int size = 100;
-    [Tooltip("這是車子的重量，單位是噸。")]
-    public float weight = 1.5f;
-    [Header("品牌")]
-    [Tooltip("這是車子的品牌")]
-    public string brand = "BMW";
-    [Header("是否有天窗"), Tooltip("這台車有沒有天窗")]
-    public bool hasWindow = true;
+
+
+
+
+ 
+
+
+
+
+
+ 
+
+    
+
 }
+  
