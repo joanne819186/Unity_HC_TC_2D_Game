519d4d1dfc82de03e8313e05a5ffd447311c8d47
KID <50666207+KID421@users.noreply.github.com>
2020-12-08 19:14:49 +0800

    備份

--- 2DGame/Assets/Scripts/Car.cs
+++ 2DGame/Assets/Scripts/Car.cs
@@ -5,7 +5,7 @@ public class Car : MonoBehaviour
     // 單行註解 (說明)
     // 數值 = 欄位 Field
     // 語法：
-    // 類型 名稱；
+    // 修飾詞 類型 名稱 (指定 值)；
 
     // 四大類型 - Unity 常用
     // 整　數 int：沒有小數點的數值，例：０、１、－９９
@@ -13,8 +13,23 @@ public class Car : MonoBehaviour
     // 字　串 string：ＢＭＷ、賓士、＠＃＄
     // 布林值 bool：有 true、沒有 false
 
-    int size;
-    float weight;
-    string brand;
-    bool hasWindow;
+    // 指定符號 =
+
+    // 私人 private 不會顯示在屬性面板上 - 預設值
+    // 公開 public  會顯示在屬性面板上
+
+
+    // 欄位的屬性
+    // 標題 Header  - 字串
+    // 提示 Tooltip - 字串
+    // 範圍 Range
+    [Header("這是車子的尺寸")]
+    public int size = 100;
+    [Tooltip("這是車子的重量，單位是噸。")]
+    public float weight = 1.5f;
+    [Header("品牌")]
+    [Tooltip("這是車子的品牌")]
+    public string brand = "BMW";
+    [Header("是否有天窗"), Tooltip("這台車有沒有天窗")]
+    public bool hasWindow = true;
 }
